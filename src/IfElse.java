7eaf64238ce32c4e741c28e3135758f3c0106d24
RenanBezerraEnes <renanbezerraenes@icloud.com>
2024-01-22 13:09:21 +0100

    If Else logic

---
+++ src/IfElse.java
@@ -0,0 +1,10 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int age = 18;
+        if (age > 19){
+            System.out.println("Welcome to my website");
+        } else {
+            System.out.println("You're not welcome to my website");
+        }
+    }
+}
