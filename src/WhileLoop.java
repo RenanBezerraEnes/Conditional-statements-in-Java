30173bee5fc65cafba527c677142a133784ea311
RenanBezerraEnes <renanbezerraenes@icloud.com>
2024-01-22 14:31:06 +0100

    While Loop in Java

---
+++ src/WhileLoop.java
@@ -0,0 +1,11 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int counter = 0;
+        while (counter < 15){
+            System.out.println("It is a beautiful day " + counter);
+            counter++;
+        }
+
+        System.out.println("STOPPED!");
+    }
+}
