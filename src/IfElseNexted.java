0e6d930ad97514985ee639d872efd2c90ec6a7b9
RenanBezerraEnes <renanbezerraenes@icloud.com>
2024-01-22 14:25:17 +0100

    nexted if else

---
+++ src/IfElseNexted.java
@@ -0,0 +1,13 @@
+public class IfElseNexted {
+    public static void main(String[] args) {
+        int age = 18;
+        if (age > 19){
+            System.out.println("Welcome to my website");
+        } else if(age == 13){
+            System.out.println("Please provide us your email id");
+        }
+        else {
+            System.out.println("You're not welcome to my website");
+        }
+    }
+}
